e8688b173d65731176faeee34283438ac1bce3d6
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-05-30 21:07:29 -0500

    Estadia controller added

---
+++ src/main/java/pe/edu/upc/api/EstadiaController.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/estadia")
+public class EstadiaController {
+	
+
+}
